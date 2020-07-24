4ae9e88cbaccd96c2aad866491afb0309bb20320
Łukasz Wieczorek <lukasz.wieczorek@ironin.pl>
2020-07-24 12:13:54 +0200

    Add fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,28 @@
+def fizzbuzz(number):
+    multiple_of_three = number % 3 == 0
+    multiple_of_five = number % 5 == 0
+    if multiple_of_three and multiple_of_five:
+        print('FizzBuzz')
+    elif multiple_of_three:
+        print('Fizz')
+    elif multiple_of_five:
+        print('Buzz')
+    else:
+        print(number)
+
+def main():
+    print('Please input n:')
+    n = input()
+    print('Please input m:')
+    m = input()
+    input_ok = 1 <= n and n < m and m <= 10000
+    if not input_ok:
+        print 'Invalid input'
+        exit(1)
+    print('Result:')
+    for number in range(n, m + 1):
+        fizzbuzz(number)
+
+
+if __name__ == '__main__':
+    main()
